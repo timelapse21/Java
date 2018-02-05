bebb107ee2c6109cb6ee5561d6aeee06448d8d48
timelapse21 <racecartim@gmail.com>
2018-02-05 17:41:32 -0500

    a

--- 02_01 Components/src/Main.java
+++ 02_01 Components/src/Main.java
@@ -1,6 +1,9 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Main {
-
+public class Main extends JFrame {
 	public static void main(String[] args) {
 		new Components();
 	}
